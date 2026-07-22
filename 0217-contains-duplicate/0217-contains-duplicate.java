a053cd3d0837ab53abc539928b3a3e769a171e0b
amruthagollavilli <amruthagollavilli@gmail.com>
2026-07-22 18:23:10 +0530

    Time: 18 ms (53.97%), Space: 108.4 MB (13.6%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int ele:nums){
+            if(set.contains(ele)){
+               return true;
+            }
+            set.add(ele);
+        }
+               return false;
+            }
+    }
