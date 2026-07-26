d6010c5ac8f80305a5f8158b1d812496e57b7f03
amruthagollavilli <amruthagollavilli@gmail.com>
2026-07-26 10:48:56 +0530

    Time: 2 ms (76.75%), Space: 43 MB (84.64%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,34 +1,23 @@
 class Solution {
     public int longestPalindrome(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i= 0;i<s.length();i++){
-            char c= s.charAt(i);
-            if(map.containsKey(s.charAt(i))){
-                map.put(c,map.get(c)+1);
-            }
-            else{
-                map.put(c,1);
-            }
-        }
+        int[] freq = new int[128];
         int length = 0;
         boolean hasOdd = false;
-
-        // Use the frequencies
-        for (int count : map.values()) {
-            if (count % 2 == 0) {
+        for(int i = 0;i<s.length();i++){
+            freq[s.charAt(i)]++;
+        }
+        for(int count:freq){
+            if(count%2 == 0){
                 length += count;
-            } else {
+            }
+            else{
                 length += count - 1;
                 hasOdd = true;
             }
         }
-
-        // One odd frequency character can be the center
-        if (hasOdd) {
+        if(hasOdd){
             length++;
         }
-
         return length;
-
     }
 }
