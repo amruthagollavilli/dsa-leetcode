8c340ab34ab8990903d387e0ce7e05cf024030ae
amruthagollavilli <amruthagollavilli@gmail.com>
2026-08-15 16:00:46 +0530

    Time: 4 ms (83.01%), Space: 46.4 MB (43.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,10 @@
-import java.util.*;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) return false;
-
-        Map<Character, Integer> map = new HashMap<>();
-
-        // Step 1: count characters in s
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        // Step 2: subtract using t
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-
-            if (!map.containsKey(ch) || map.get(ch) == 0) {
-                return false;
-            }
-
-            map.put(ch, map.get(ch) - 1);
-        }
-
-        return true;
+       if(s.length() != t.length()) return false; 
+       char[] a = s.toCharArray();
+       char[] b = t.toCharArray();
+       Arrays.sort(a);
+       Arrays.sort(b);
+       return Arrays.equals(a,b);
     }
 }
