0da9796e62f46e2f3ea35f7d8f8afbcff7fa8199
amruthagollavilli <amruthagollavilli@gmail.com>
2026-09-10 23:00:34 +0530

    Time: 3 ms (86.05%), Space: 43.1 MB (85.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        char[] brackets = s.toCharArray();
-        Stack<Character> st = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                st.push(ch);
-            } else {
-                if (st.isEmpty()) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{' ){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
                     return false;
                 }
-
-                // Pop the top element and see if it matches the correct pairs
-                char top = st.pop();
-                if (ch == ')' && top != '(')
+                char top = stack.pop();
+                if(ch == ')' && top != '('){
                     return false;
-                if (ch == ']' && top != '[')
+                }
+                if(ch == ']' && top != '['){
                     return false;
-                if (ch == '}' && top != '{')
+                }
+                if(ch == '}' && top != '{'){
                     return false;
+                }
             }
         }
-        // If the stack is completely empty, all brackets were matched perfectly
-        return st.isEmpty();
+        return stack.isEmpty();
     }
 }
